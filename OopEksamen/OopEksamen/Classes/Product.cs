ff849543cc1aa1b1fc3ace5badf702e3ecf40796
Henrik Ginnerup <hginnerup@hotmail.com>
2020-11-20 14:09:58 +0100

    Added Product

---
+++ OopEksamen/OopEksamen/Classes/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopEksamen.Classes
+{
+    public class Product
+    {
+        public uint ID { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+        public int Price { get; set; }
+        public bool CanBeBoughtOnCredit { get; set; }
+        public DateTime SeasonStartDate { get; set; }
+        public DateTime SeasonEndDate { get; set; }
+    }
+}
