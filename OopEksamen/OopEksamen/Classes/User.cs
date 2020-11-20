602f1c4f4d94fd811a1d9ac76302b57494803024
Henrik Ginnerup <hginnerup@hotmail.com>
2020-11-20 14:03:45 +0100

    Added User

---
+++ OopEksamen/OopEksamen/Classes/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopEksamen.Classes
+{
+    public class User : IComparable
+    {
+        public int ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public int Balance { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Firstname} {Lastname} ({Email})";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (obj.GetType() != this.GetType()) return false;
+            // obj is an instance of User
+
+            return (obj as User).ID == ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return ID.CompareTo(obj);
+        }
+    }
+}
