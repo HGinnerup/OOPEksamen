6ff8728c0b8bce46c6137027b066112936fa9a94
Henrik Ginnerup <hginnerup@hotmail.com>
2020-11-20 15:37:35 +0100

    Added FormatCredits

---
+++ OopEksamen/OopEksamen/Classes/Utility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopEksamen.Classes
+{
+    public static class Utility
+    {
+        public static string FormatCredits(int balance)
+        {
+            return $"{((decimal)balance) / 100} DKK";
+        }
+    }
+}
