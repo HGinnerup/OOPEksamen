421d0748c9596c55a55ac37c359afaa10ab58b46
Henrik Ginnerup <hginnerup@hotmail.com>
2020-11-22 20:21:18 +0100

    Init StregsystemCLI

---
+++ OopEksamen/StregsystemCLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StregsystemCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
