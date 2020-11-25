821ef558b2ed8d983b2c773a81a56a410230b496
Henrik Ginnerup <hginnerup@hotmail.com>
2020-11-25 16:17:57 +0100

    Added Project to parse old data

---
+++ OopEksamen/UpdateOldData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UpdateOldData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
